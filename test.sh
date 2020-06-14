ef34ff6d9f8eb33a48ea5519360a7473404ac6a1
jpdump0 <66913268+jpdump0@users.noreply.github.com>
2020-06-14 23:49:23 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
-echo helloworld2
-cat 123
-echo 123
+#!/bin/bash
+# Run as: bash <(curl -s https://raw.githubusercontent.com/jpdump0/termux-ytdl/master/test.sh)
+
+set -e
+pkg install python ffmpeg
+pip install youtube-dl
+termux-setup-storage
+echo "Setup successful!"
