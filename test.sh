20a25347006d31d9d580f79f344c7ef54a9d663c
jpdump0 <66913268+jpdump0@users.noreply.github.com>
2020-06-14 23:58:58 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
 # Run as: bash <(curl -s https://raw.githubusercontent.com/jpdump0/termux-ytdl/master/test.sh)
 
 set -e
-pkg install python ffmpeg
+pkg install -y python ffmpeg
 pip install youtube-dl
 termux-setup-storage
 echo "Setup successful!"
